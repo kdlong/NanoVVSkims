5d207227662eeb9487c15c76ff36cd8a1a7e975c
Kenneth Long <kdlong@hep.wisc.edu>
2019-06-12 03:56:09 -0400

    Fix crab script, which pointed to example module

--- Dilepton/dilepton_postproc.py
+++ Dilepton/dilepton_postproc.py
@@ -9,7 +9,7 @@ from dileptonModule import *
 
 #testfile = os.environ["CMSSW_BASE"] + "/src/Analysis/VVAnalysis/test/9D8BBBB2-187D-CE49-9CAE-009ADB180509.root"
 #testfile = "/afs/hep.wisc.edu/cms/kdlong/WZAnalysis/CMSSW_10_4_0_patch1/src/Analysis/VVAnalysis/test/9D8BBBB2-187D-CE49-9CAE-009ADB180509.root"
-testfile = "3653AD29-7BA8-E811-BCE8-A0369FE2C092.root"
+testfile = "/data/kelong/NanoAODSkims/Unskimmed/44FD6740-8FBD-754E-84D7-3FE57537AEA9.root"
 
 p=PostProcessor(".",[testfile],"(nElectron>1 || nMuon > 1)","keep_and_drop.txt",[dileptonModuleConstr()],provenance=True)
 p.run()
